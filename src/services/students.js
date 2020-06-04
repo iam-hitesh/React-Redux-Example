7abfab0d4e8408c83b21857158e095d5e1f3a4cb
Hitesh Yadav <hhiteshyadav@gmail.com>
2020-06-04 20:01:14 +0530

    API_URL updated

--- src/services/students.js
+++ src/services/students.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 export const getStudents = async () => {
-    return await axios.get(
-        `http://hatchways.io/api/assessment/students`
-    );
+    let API_URL;
+    if (process.env.NODE_ENV === 'production') {
+        API_URL = 'https://www.hatchways.io/api/assessment/students';
+    } else {
+        API_URL = 'http://hatchways.io/api/assessment/students';
+    }
+
+    return await axios.get(API_URL);
 };
